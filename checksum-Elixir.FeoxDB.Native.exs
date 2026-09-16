ddfb676488ed09da2e5096bf92861927d136713b
Ziinc <22714384+Ziinc@users.noreply.github.com>
2026-09-16 21:54:22 +0000

    Update NIF checksums for v0.1.1

---
+++ checksum-Elixir.FeoxDB.Native.exs
@@ -0,0 +1,8 @@
+%{
+  "libfeoxdb_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:85ff321286186288d00c4a4c0354de07e5e4b10ef337f4f1dddf0793168fde7d",
+  "libfeoxdb_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:01ae729d64b3dfa3afcb73faf3f11fb589ae4c52fce9b7679c0af39cff76b097",
+  "libfeoxdb_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:e8891b956c578b6bd34b34731ef338e6e1f7e5bd358aa216fca8a9770aa135f1",
+  "libfeoxdb_nif-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:8b7491b205ac55c0bfa5df2364d506dd4f3afa161a525a583fcd182e49b0913c",
+  "libfeoxdb_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d4ef78e8642647c9656491c531e9502d122ae8fe3f54aed62f104ce2f4eab185",
+  "libfeoxdb_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:20453948be80ecd12626be3c1006fb02b239ee46d62cbe521770ab57122e4c2c",
+}
